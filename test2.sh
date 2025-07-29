08f9157db0881f1ed3161167c5a12b3394318971
David Williams-King <d.williams-king@saifh.org>
2025-07-29 12:39:28 -0400

    new sysprompts

--- test2.sh
+++ test2.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-DATASET_SHORT=D5
+for dataset in D6 D7; do
+    for sysprompt in linh2; do
+        for steps in $(seq 5 5 30); do
+        #for steps in $(seq 10 10 100); do
+            # modes: all, train, basic_test, eval
+            mode=all
+            project=LAT-$dataset-$sysprompt-$steps
 
-for sysprompt in orig adam adam2; do
-    for steps in $(seq 10 10 100); do
-        # modes: all, train, basic_test, eval
-        mode=all
-        project=LAT-$DATASET_SHORT-$sysprompt-$steps
+            echo Submitting $project: dataset $dataset, sysprompt $sysprompt, steps $steps, mode $mode
 
-        echo Submitting $project: dataset $DATASET_SHORT, sysprompt $sysprompt, steps $steps, mode $mode
-
-        ./submit_any.sh $project \
-            --cache-dir cache \
-            --data-folder dataset/$DATASET_SHORT/ \
-            --project-name $project \
-            --system-prompt-file sysprompt/$sysprompt.txt \
-            --num-steps $steps \
-            --mode $mode
+            ./submit_any.sh $project \
+                --cache-dir cache \
+                --data-folder dataset/$dataset/ \
+                --project-name $project \
+                --system-prompt-file sysprompt/$sysprompt.txt \
+                --num-steps $steps \
+                --mode $mode
+        done
     done
 done
