a673808ae05c69220704b3fc1857e4991462b87c
David Williams-King <d.williams-king@saifh.org>
2025-07-28 23:39:24 -0400

    better script

--- test1.sh
+++ test1.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 # for linh
 
-# modes: all, train, basic_test, eval
+SYSPROMPT_SHORT=adam
+DATASET_SHORT=D5
 
-time python lat_personality.py --cache-dir cache --data-folder dataset/D5/ --project-name LAT-D5-adam --mode all --num-steps 10
+for steps in $(seq 10 10 100); do
+    # modes: all, train, basic_test, eval
+    mode=all
+    project=LAT-$DATASET_SHORT-$SYSPROMPT_SHORT-$steps
+
+    echo ======== Running $project: dataset $DATASET_SHORT, sysprompt $SYSPROMPT_SHORT, steps $steps, mode $mode
+
+    time python lat_personality.py \
+        --cache-dir cache \
+        --data-folder dataset/$DATASET_SHORT/ \
+        --project-name $project \
+        --system-prompt-file sysprompt/$SYSPROMPT_SHORT.txt \
+        --num-steps $steps \
+        --mode $mode
+done
