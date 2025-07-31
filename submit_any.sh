0749c9c56ce4cf9a39f56ec335e36e606351b749
Thai.Linh Le <let@cn-l059.server.mila.quebec>
2025-07-31 08:51:57 -0400

    update for let@ account

--- submit_any.sh
+++ submit_any.sh
@@ -3,20 +3,20 @@ PROJECT_NAME=$1
 shift
 OTHER_ARGS="$@"
 
-CACHE_DIR=/network/scratch/d/d.williams-king/latent-adversarial-training-personality/cache
+CACHE_DIR=/network/scratch/l/let/projects/latent-adversarial-training-personality/cache
 
 sbatch <<EOF
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --job-name=lat-$PROJECT_NAME
-#SBATCH --time=48:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=10
 #SBATCH --gres=gpu:l40s
-#SBATCH -o job/lat-$PROJECT_NAME.out
-#SBATCH -e job/lat-$PROJECT_NAME.err
+#SBATCH -o job/$PROJECT_NAME.out
+#SBATCH -e job/$PROJECT_NAME.err
 
-cd /network/scratch/d/d.williams-king/latent-adversarial-training-personality
+cd /network/scratch/l/let/projects/latent-adversarial-training-personality
 
 module load cuda/12.6.0/cudnn/9.3
 
@@ -24,8 +24,9 @@ export PYTHONBREAKPOINT=0
 export WANDB_MODE=disabled
 
 export PYTHONPATH=.
-. /network/scratch/d/d.williams-king/ccc.sh
-conda activate lat
+
+module load anaconda
+conda activate /network/scratch/l/let/envs/lat
 
 python lat_personality.py $OTHER_ARGS
 EOF
