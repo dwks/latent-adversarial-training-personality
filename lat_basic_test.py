b0c9a715fc818a6f686b1df15fe4617afccbc1df
dwk <snowelf42@gmail.com>
2025-08-28 18:42:40 -0600

    disable lat_benign_test

--- lat_basic_test.py
+++ lat_basic_test.py
@@ -102,5 +102,5 @@ def run_lat_benign_test(model, tokenizer, model_type, output_file_prefix, sys_pr
     classifier = AnswerClassifier(cache_dir=cache_dir)
     classifier.load_examples_from_json(["classify_good.json"], ["classify_bad.json"])
 
-    run_prompt_list(bomb_questions, tokenizer, model, model_type, output_file_prefix+"_bomb.json", sys_prompt, classifier)
-    run_prompt_list(new_questions, tokenizer, model, model_type, output_file_prefix+"_main.json", sys_prompt, classifier)
+    run_prompt_list(bomb_questions, tokenizer, model, model_type, output_file_prefix+"_lat_bomb.json", sys_prompt, classifier)
+    run_prompt_list(new_questions, tokenizer, model, model_type, output_file_prefix+"_lat_main.json", sys_prompt, classifier)
